af189ab470389efb3df0c5a799fb1d290417dace
Karan <kc2532002@gmail.com>
2024-03-08 13:47:52 +0530

    methods

--- Javascript/Namastey JS/callApplyBind.js
+++ Javascript/Namastey JS/callApplyBind.js
@@ -1,18 +1,28 @@
 let name = {
     firstName : `Karan`,
     lastName : `Chourasia`,
-    printName : function () { 
-        console.log(`${this.firstName} ${this.lastName}`);
-    }
 };
-name.printName();
+
+let printName =  function (hometown, state) { 
+    console.log(`${this.firstName} ${this.lastName} from ${hometown}, ${state}`);
+}
+printName.call(name,"Delhi","New Delhi");
 
 const name2 = {
     firstName: `Mahendra`,
     lastName: `Dhoni`
 }
 
-//function borrowing
+//function borrowing -> borrowing functions other objects
 //call method 
-name.printName.call(name2);
+printName.call(name2,"Andheri","Mumbai");
 
+//Apply method
+//->alomost like apply method we pass the parameters in arraylist
+printName.apply(name2, ["Mumbai","Maharashtra" ]);
+
+
+//bind method  -> gives you a copy that can be invoked later
+let printMyName = printName.bind(name2, "Dehradun", "Uttarakhand");
+console.log(printMyName);
+printMyName();
