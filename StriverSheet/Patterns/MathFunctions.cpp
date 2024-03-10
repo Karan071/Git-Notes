d9ad52a664f402f8c9f1a459c4ef3e0a705e9429
Karan <kc2532002@gmail.com>
2024-03-10 16:02:01 +0530

    gcd

--- StriverSheet/Patterns/MathFunctions.cpp
+++ StriverSheet/Patterns/MathFunctions.cpp
@@ -13,12 +13,41 @@ int countEvenDigit(int n){
     while(temp > 0){
         int lastDigit = temp % 10;
         temp = temp / 10;
-        if(n % lastDigit == 0) count++;
+        if(lastDigit != 0 && n % lastDigit == 0) count++;
     }
     return count;
 }
+int reverseDigit(int n){
+    int temp = n, revDigit = 0;
+    while(temp > 0){
+        int lastDigit = temp % 10;
+        revDigit  = (revDigit * 10) + lastDigit;
+        temp = temp / 10;
+    }
+    return revDigit;
+
+}
+bool checkPalindrome(int num){
+    int temp = num, revDigit = 0;
+    while(temp > 0) {
+        int lastDigit = temp % 10;
+        revDigit = revDigit * 10 + lastDigit;
+        temp /= 10;
+    }
+    return (num == revDigit);
+}
+int naiveGcd(int num1, int num2){
+    int result = 0;
+    for(int i = 1; i <= min(num1,num2); i++){
+        if(num1 % i == 0 && num2 % i == 0){
+            result = i;
+        }
+    }
+    return result;
+}
 int main(){
-    int n;
-    cin >> n;
-    cout << countDigits(n);
+    int n,x;
+    cin >> n >> x;
+    
+
 }
